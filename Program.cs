4073541b18c3724e7c645705ed53bdecb0501c2c
Heather Johnson-Ledlie <heatherjohnson120@yahoo.com>
2019-06-16 19:57:41 -0400

    updating data

--- Program.cs
+++ Program.cs
@@ -56,6 +56,24 @@ namespace SafariVacation
       // {
       //   Console.Writeline(animal.Species);
       // } 
+      Console.WriteLine("Let's go on a Safari!");
+      Console.WriteLine("Wow you've seen...");
+
+
+      var alreadySeen = db.Animal.OrderBy(SeenAnimals => SeenAnimals.Species);
+      foreach (var SeenAnimals in alreadySeen)
+      {
+        Console.WriteLine($"{SeenAnimals.Species}");
+      }
+      Console.WriteLine(":=========================:");
+
+      var elephant = db.Animal.FirstOrDefault(SeenAnimals => SeenAnimals.Species == "Elephant");
+      elephant.CountOfTimesSeen = elephant.CountOfTimesSeen + 2;
+      db.SaveChanges();
+
+      var e = db.Animal.FirstOrDefault(SeenAnimals => SeenAnimals.Species == "Elephant");
+      elephant.LocationOfLastSeen = elephant.LocationOfLastSeen + "Sahara";
+      db.SaveChanges();
 
 
     }
