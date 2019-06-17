cb0d180bb444b1e7982e06035d431ac41c8f5356
Heather Johnson-Ledlie <heatherjohnson120@yahoo.com>
2019-06-16 21:04:33 -0400

    Animals Added page added

--- Program.cs
+++ Program.cs
@@ -7,55 +7,9 @@ namespace SafariVacation
   {
     static void Main(string[] args)
     {
+
       var db = new SafariVacationContext();
 
-      db.Animal.Add(new SeenAnimals
-      {
-        Species = "Elephant",
-        CountOfTimesSeen = 3,
-        LocationOfLastSeen = "Desert"
-      });
-      db.SaveChanges();
-      db.Animal.Add(new SeenAnimals
-      {
-        Species = "Lion",
-        CountOfTimesSeen = 1,
-        LocationOfLastSeen = "Desert"
-      });
-      db.SaveChanges();
-      db.Animal.Add(new SeenAnimals
-      {
-        Species = "Tiger",
-        CountOfTimesSeen = 5,
-        LocationOfLastSeen = "Desert"
-      });
-      db.SaveChanges();
-      db.Animal.Add(new SeenAnimals
-      {
-        Species = "Bear",
-        CountOfTimesSeen = 7,
-        LocationOfLastSeen = "Jungle"
-      });
-      db.SaveChanges();
-      db.Animal.Add(new SeenAnimals
-      {
-        Species = "Zebra",
-        CountOfTimesSeen = 12,
-        LocationOfLastSeen = "Desert"
-      });
-      db.SaveChanges();
-      db.Animal.Add(new SeenAnimals
-      {
-        Species = "Rhino",
-        CountOfTimesSeen = 9,
-        LocationOfLastSeen = "desert"
-      });
-      db.SaveChanges();
-
-      // foreach (var animal in SeenAnimals)
-      // {
-      //   Console.Writeline(animal.Species);
-      // } 
       Console.WriteLine("Let's go on a Safari!");
       Console.WriteLine("Wow you've seen...");
 
