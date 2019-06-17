e933681d1ab11f633b9f84e9ae24fd18fc37810f
Heather Johnson-Ledlie <heatherjohnson120@yahoo.com>
2019-06-17 12:50:33 -0400

    removed desert animals

--- AnimalsAdded.cs
+++ AnimalsAdded.cs
@@ -1,58 +1,58 @@
-using System;
-using System.Linq;
+// using System;
+// using System.Linq;
 
 
-namespace SafariVacation
-{
-  public class AnimalsAdded
-  {
+// namespace SafariVacation
+// {
+//   public class AnimalsAdded
+//   {
 
-    static void Main(string[] args)
-    {
-      var db = new SafariVacationContext();
+//     static void (string[] args)
+//     {
+//       var db = new SafariVacationContext();
 
-      db.Animal.Add(new SeenAnimals
-      {
-        Species = "Elephant",
-        CountOfTimesSeen = 3,
-        LocationOfLastSeen = "Desert"
-      });
-      db.SaveChanges();
-      db.Animal.Add(new SeenAnimals
-      {
-        Species = "Lion",
-        CountOfTimesSeen = 1,
-        LocationOfLastSeen = "Desert"
-      });
-      db.SaveChanges();
-      db.Animal.Add(new SeenAnimals
-      {
-        Species = "Tiger",
-        CountOfTimesSeen = 5,
-        LocationOfLastSeen = "Desert"
-      });
-      db.SaveChanges();
-      db.Animal.Add(new SeenAnimals
-      {
-        Species = "Bear",
-        CountOfTimesSeen = 7,
-        LocationOfLastSeen = "Jungle"
-      });
-      db.SaveChanges();
-      db.Animal.Add(new SeenAnimals
-      {
-        Species = "Zebra",
-        CountOfTimesSeen = 12,
-        LocationOfLastSeen = "Desert"
-      });
-      db.SaveChanges();
-      db.Animal.Add(new SeenAnimals
-      {
-        Species = "Rhino",
-        CountOfTimesSeen = 9,
-        LocationOfLastSeen = "desert"
-      });
-      db.SaveChanges();
-    }
-  }
-}
+//     db.Animal.Add(new SeenAnimals
+//       {
+//         Species = "Elephant",
+//         CountOfTimesSeen = 3,
+//         LocationOfLastSeen = "Desert"
+//       });
+//       db.SaveChanges();
+//       db.Animal.Add(new SeenAnimals
+//       {
+//         Species = "Lion",
+//         CountOfTimesSeen = 1,
+//         LocationOfLastSeen = "Desert"
+//       });
+//       db.SaveChanges();
+//       db.Animal.Add(new SeenAnimals
+//       {
+//         Species = "Tiger",
+//         CountOfTimesSeen = 5,
+//         LocationOfLastSeen = "Desert"
+//       });
+//       db.SaveChanges();
+//       db.Animal.Add(new SeenAnimals
+//       {
+//         Species = "Bear",
+//         CountOfTimesSeen = 7,
+//         LocationOfLastSeen = "Jungle"
+//       });
+//       db.SaveChanges();
+//       db.Animal.Add(new SeenAnimals
+//       {
+//         Species = "Zebra",
+//         CountOfTimesSeen = 12,
+//         LocationOfLastSeen = "Desert"
+//       });
+//       db.SaveChanges();
+//       db.Animal.Add(new SeenAnimals
+//       {
+//         Species = "Rhino",
+//         CountOfTimesSeen = 9,
+//         LocationOfLastSeen = "desert"
+//       });
+//       db.SaveChanges();
+//     }
+//   }
+// }
--- Program.cs
+++ Program.cs
@@ -10,6 +10,50 @@ namespace SafariVacation
 
       var db = new SafariVacationContext();
 
+
+      // db.Animal.Add(new SeenAnimals
+      //   {
+      //     Species = "Elephant",
+      //     CountOfTimesSeen = 3,
+      //     LocationOfLastSeen = "Desert"
+      //   });
+      //   db.SaveChanges();
+      //   db.Animal.Add(new SeenAnimals
+      //   {
+      //     Species = "Lion",
+      //     CountOfTimesSeen = 1,
+      //     LocationOfLastSeen = "Desert"
+      //   });
+      //   db.SaveChanges();
+      //   db.Animal.Add(new SeenAnimals
+      //   {
+      //     Species = "Tiger",
+      //     CountOfTimesSeen = 5,
+      //     LocationOfLastSeen = "Desert"
+      //   });
+      //   db.SaveChanges();
+      //   db.Animal.Add(new SeenAnimals
+      //   {
+      //     Species = "Bear",
+      //     CountOfTimesSeen = 7,
+      //     LocationOfLastSeen = "Jungle"
+      //   });
+      //   db.SaveChanges();
+      //   db.Animal.Add(new SeenAnimals
+      //   {
+      //     Species = "Zebra",
+      //     CountOfTimesSeen = 12,
+      //     LocationOfLastSeen = "Desert"
+      //   });
+      //   db.SaveChanges();
+      //   db.Animal.Add(new SeenAnimals
+      //   {
+      //     Species = "Rhino",
+      //     CountOfTimesSeen = 9,
+      //     LocationOfLastSeen = "desert"
+      //   });
+      //   db.SaveChanges();
+
       Console.WriteLine("Let's go on a Safari!");
       Console.WriteLine("Wow you've seen...");
 
@@ -21,13 +65,23 @@ namespace SafariVacation
       }
       Console.WriteLine(":=========================:");
 
-      var elephant = db.Animal.FirstOrDefault(SeenAnimals => SeenAnimals.Species == "Elephant");
-      elephant.CountOfTimesSeen = elephant.CountOfTimesSeen + 2;
-      db.SaveChanges();
+      // var elephant = db.Animal.FirstOrDefault(SeenAnimals => SeenAnimals.Species == "Elephant");
+      // elephant.CountOfTimesSeen = elephant.CountOfTimesSeen + 2;
+      // db.SaveChanges();
 
-      var e = db.Animal.FirstOrDefault(SeenAnimals => SeenAnimals.Species == "Elephant");
-      elephant.LocationOfLastSeen = elephant.LocationOfLastSeen + "Sahara";
-      db.SaveChanges();
+      // var e = db.Animal.FirstOrDefault(SeenAnimals => SeenAnimals.Species == "Elephant");
+      // elephant.LocationOfLastSeen = "Sahara";
+      // db.SaveChanges();
+
+      var j = db.Animal.Where(SeenAnimals => SeenAnimals.LocationOfLastSeen == "Jungle");
+      foreach (var SeenAnimals in j)
+      {
+        Console.WriteLine($"{SeenAnimals.Species}");
+      }
+
+      // var desertAnimals = db.Animal.Where(SeenAnimals => SeenAnimals.LocationOfLastSeen == "Desert");
+      // db.Animal.RemoveRange(desertAnimals);
+      // db.SaveChanges();
 
 
     }
